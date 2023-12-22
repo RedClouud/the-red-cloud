54bdbd4976c7adf6296220d5190042c8baff55df
RedClouud <jackchristie87@gmai.com>
2023-12-22 15:57:31 +0000

    Fixed `.gitignore`, leaned down pihole script

--- pihole/start-pihole.sh
+++ pihole/start-pihole.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# https://github.com/pi-hole/docker-pi-hole/blob/master/README.md
+# Forked from https://github.com/pi-hole/docker-pi-hole/blob/master/examples/docker_run.sh
 
-
+password=$1
 
 # Note: FTLCONF_LOCAL_IPV4 should be replaced with your external ip.
 docker run -d \
@@ -10,12 +10,8 @@ docker run -d \
     -p 53:53/tcp -p 53:53/udp \
     -p 80:80 \
     -e TZ="Europe/London" \
-    -v "$(pwd)/etc-pihole:/etc/pihole" \
-    -v "$(pwd)/etc-dnsmasq.d:/etc/dnsmasq.d" \
-    --dns=127.0.0.1 --dns=1.1.1.1 \
+    -v "./etc-pihole:/etc/pihole" \
+    -v "./etc-dnsmasq.d:/etc/dnsmasq.d" \
     --restart=unless-stopped \
-    --hostname pi.hole \
-    -e VIRTUAL_HOST="pi.hole" \
-    -e PROXY_LOCATION="pi.hole" \
-    -e FTLCONF_LOCAL_IPV4="127.0.0.1" \
+    -e WEBPASSWORD=$password \
     pihole/pihole:latest
